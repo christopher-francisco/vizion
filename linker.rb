0bc3e61fe6686929d804ae52fd4c0160b182dbd0
Christopher Francisco <christopher.f.almanza@gmail.com>
2018-08-04 14:40:08 -0500

    Supress symlink output

--- linker.rb
+++ linker.rb
@@ -3,14 +3,14 @@ class Linker
     if Dir.exist?(symlink) || File.exists?(symlink) || File.symlink?(symlink)
       return false
     else
-      Rake::FileUtilsExt.ln_s original, symlink
+      Rake::FileUtilsExt.ln_s original, symlink, verbose: false
       return true
     end
   end
 
   def unlink symlink
     if File.symlink? symlink
-      Rake::FileUtilsExt.safe_unlink symlink
+      Rake::FileUtilsExt.safe_unlink symlink, verbose: false
       return true
     else
       return false
