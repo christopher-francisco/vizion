9e8d528ea2a349901ab20a6470817909951004cd
Christopher Francisco <christopher.f.almanza@gmail.com>
2020-03-07 20:51:24 -0500

    Adds confirmation to tokens setup

--- scripts/setup_tokens.sh
+++ scripts/setup_tokens.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
+promt_overwrite_filename() {
+  local filename=$1
 
-read -s -p "Github cloud token (repo, gist permissions): " github_cloud_token
-echo
+  read -p "File $filename already exists. Overwrite (y/N): " overwrite
 
-read -p "Github enterprise username: " github_enterprise_username
-read -p "Github enterprise hostname: " github_enterprise_hostname
-read -s -p "Github enterprise token (repo, gist permissions): " github_enterprise_token
-echo
+  if [[ $overwrite = "y" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
-echo "
-export HOMEBREW_GITHUB_API_TOKEN='$github_cloud_token'
+main() {
+  filename=~/.zshrc.tokens
+
+  if [[ -e $filename ]]; then
+    promt_overwrite_filename $filename
+    if [[ $? -gt 0 ]]; then
+      return 1
+    fi
+  fi
+
+  read -s -p "Github cloud token (repo, gist permissions): " github_cloud_token
+  echo
+
+  read -p "Github enterprise username: " github_enterprise_username
+  read -p "Github enterprise hostname: " github_enterprise_hostname
+  read -s -p "Github enterprise token (repo, gist permissions): " github_enterprise_token
+  echo
+
+  echo "export HOMEBREW_GITHUB_API_TOKEN='$github_cloud_token'
 
 # ----
 # Used by ~/.netrc
@@ -27,4 +47,9 @@ useCloudGitHub() {
 useEnterpriseGitHub() {
     export GITHUB_TOKEN='$github_enterprise_token'
     export GITHUB_HOST='$github_enterprise_hostname'
-}" > ~/.zshrc.tokens
+}" > $filename
+
+echo "Created $filename"
+}
+
+main
