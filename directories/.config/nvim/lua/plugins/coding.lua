a0eb1b46d6c73ce044b9b7052d68850b6b81fd97
Christopher Francisco <christopher.f.almanza@gmail.com>
2024-09-15 20:37:05 +0100

    Configure dap, xcodebuild, swiftformat and swiftlint

--- directories/.config/nvim/lua/plugins/coding.lua
+++ directories/.config/nvim/lua/plugins/coding.lua
@@ -100,14 +100,14 @@ return {
       modes = { insert = true, command = false, terminal = false },
 
       -- skip autopair when next character is one of these
-      -- skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
 
       -- skip autopair when the cursor is inside these treesitter nodes
-      -- skip_ts = { "string" },
+      skip_ts = { "string" },
 
       -- skip autopair when next character is closing pair
       -- and there are more closing pairs than opening pairs
-      -- skip_unbalanced = true,
+      skip_unbalanced = true,
 
       -- better deal with markdown code blocks
       markdown = true,
